2e21417aa13ebc9ab56596c69910cbf7555305b1
Arsene Severin Boundaone <arsene.boundaone@gmail.com>
2022-07-05 11:04:50 -0500

    adding mint api unfinished code

--- mintapi/mintapi.py
+++ mintapi/mintapi.py
@@ -3,8 +3,10 @@ from dateutil.relativedelta import relativedelta
 from mintapi import constants
 import logging
 import os
-from mintapi.signIn import sign_in, _create_web_driver_at_mint_com
+from mintapi import sign_in, _create_web_driver_at_mint_com
 
+
+'''API login gather from'''
 class MintException(Exception):
     pass
 
@@ -71,75 +73,9 @@ class Mint(object):
         self.driver = None
         self.status_message = None
 
-        if email and password:
-            self.login_and_get_token(
-                email,
-                password,
-                mfa_method=mfa_method,
-                mfa_token=mfa_token,
-                mfa_input_callback=mfa_input_callback,
-                intuit_account=intuit_account,
-                headless=headless,
-                session_path=session_path,
-                imap_account=imap_account,
-                imap_password=imap_password,
-                imap_server=imap_server,
-                imap_folder=imap_folder,
-                wait_for_sync=wait_for_sync,
-                wait_for_sync_timeout=wait_for_sync_timeout,
-                fail_if_stale=fail_if_stale,
-                use_chromedriver_on_path=use_chromedriver_on_path,
-                chromedriver_download_path=chromedriver_download_path,
-                driver=driver,
-                beta=beta,
-            )
-
-    def _get_api_key_header(self):
-        key_var = "window.__shellInternal.appExperience.appApiKey"
-        api_key = self.driver.execute_script("return " + key_var)
-        auth = "Intuit_APIKey intuit_apikey=" + api_key
-        auth += ", intuit_apikey_version=1.0"
-        header = {"authorization": auth}
-        header.update(constants.JSON_HEADER)
-        return header
-
-    def close(self):
-        """Logs out and quits the current web driver/selenium session."""
-        if not self.driver:
-            return
-
-        self.driver.quit()
-        self.driver = None
-
-    def get(self, url, **kwargs):
-        return self.driver.request("GET", url, **kwargs)
-
-    def post(self, url, **kwargs):
-        return self.driver.request("POST", url, **kwargs)
-
-    def login_and_get_token(
-        self,
-        email,
-        password,
-        mfa_method=None,
-        mfa_token=None,
-        mfa_input_callback=None,
-        intuit_account=None,
-        headless=False,
-        session_path=None,
-        imap_account=None,
-        imap_password=None,
-        imap_server=None,
-        imap_folder=None,
-        wait_for_sync=True,
-        wait_for_sync_timeout=5 * 60,
-        fail_if_stale=False,
-        use_chromedriver_on_path=False,
-        chromedriver_download_path=os.getcwd(),
-        driver=None,
-        beta=False,
-    ):
-
+        # if email and password:
+        #     self.login_and_get_token()
+               
 
 def get_accounts(email, password, get_detail=False):
     mint = Mint(email, password)
